2a95a65c819c894919556c2ff790c42f0e8e3bd1
Gerald Estadieu <gestadieu@gmail.com>
2020-05-09 21:40:31 +0800

    initial v

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 Raspberry Pi-based controller for a simple soundscape installation
+
+
+install
+```js
+npm install
+```
+
+Interesting resources
+
+NPM Packages
+- https://github.com/fivdi/spi-device 
+- https://github.com/fivdi/mcp-spi-adc 
+- https://github.com/fivdi/i2c-bus
+- https://github.com/fivdi/pigpio 
+
+PM2
+- https://medium.com/@andrew.nease.code/set-up-a-self-booting-node-js-eb56ebd05549
+
+MCP3008
+- https://github.com/fivdi/mcp-spi-adc
+- https://mikaelleven.wordpress.com/2015/12/13/getting-started-with-spi-analog-input-in-nodejs-using-raspberry-pi-and-mcp3008/
+
+AUDIO
+- https://jwarren.co.uk/blog/audio-on-the-raspberry-pi-with-node-js/
+
+Web UI
+socket.io
+
+LED Strip
+- https://dordnung.de/raspberrypi-ledstrip/
--- helloworld.js
+++ helloworld.js
@@ -6,6 +6,42 @@ const board = new five.Board({
 });
 
 board.on('ready', () => {
-    const led = new five.Led('P1-7');
+    // GPIO18 - Physical Pin P1-12 - Wiring Pi 1
+    const led = new five.Led('P1-12');
     led.strobe(500);
+
+
+
+    bumper = new five.Button(7);
+    led = new five.Led(13);
+    button = new five.Button(2);
+    // Button Event API
+
+  // "down" the button is pressed
+  button.on("down", function() {
+    console.log("down");
+  });
+
+  // "hold" the button is pressed for specified time.
+  //        defaults to 500ms (1/2 second)
+  //        set
+  button.on("hold", function() {
+    console.log("hold");
+  });
+
+  // "up" the button is released
+  button.on("up", function() {
+    console.log("up");
+  });
+
+
+  bumper.on("hit", function() {
+
+    led.on();
+
+  }).on("release", function() {
+
+    led.off();
+
+  });
 })
