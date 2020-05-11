4be00df29025d38e4ae35f29bd92e43f348dce1f
Gerald Estadieu <gestadieu@gmail.com>
2020-05-11 10:04:47 +0800

    test omxplayer

--- video.js
+++ video.js
@@ -1,15 +1,49 @@
-"use strict";
-exports.__esModule = true;
-var omxplayer_node_1 = require("omxplayer-node");
-var omxplayer = omxplayer_node_1["default"]({
-    display: omxplayer_node_1.VideoOutput.HDMI0,
-    audio: omxplayer_node_1.AudioOutput.jack
+// import createVideoPlayer, { AudioOutput, VideoOutput } from "omxplayer-node";
+const OMX = require("omxplayer-node");
+
+const omxplayer = OMX({
+  display: OMX.VideoOutput.HDMI0,
+  audio: OMX.AudioOutput.jack,
 });
+
+// const omxplayer = createVideoPlayer({
+//   display: VideoOutput.HDMI0,
+//   audio: AudioOutput.jack,
+// });
+
 omxplayer.open({
-    source: "videos/1-01.mp4",
-    audio: omxplayer_node_1.AudioOutput.HDMI,
-    osd: true
+  source: "videos/1-01.mp4",
+  audio: OMX.AudioOutput.HDMI,
+  osd: true,
 });
+
+// from typescript
+// "use strict";
+// exports.__esModule = true;
+// var omxplayer_node_1 = require("omxplayer-node");
+// var omxplayer = omxplayer_node_1["default"]({
+//   display: omxplayer_node_1.VideoOutput.HDMI0,
+//   audio: omxplayer_node_1.AudioOutput.jack,
+// });
+// omxplayer.open({
+//   source: "videos/1-01.mp4",
+//   audio: omxplayer_node_1.AudioOutput.HDMI,
+//   osd: true,
+// });
+
+// omxplayer.pause();
+// omxplayer.volUp();
+// omxplayer.quit();
+
+// const omx = require("node-omxplayer");
+// const player = omx();
+// player.newSource("videos/2-01.mp4");
+
+// let player = omx('videos/1-01.mp4');
+
+// player.play();
+// player.quit();
+
 // setTimeout(() => {
 //   player.quit();
 // }, 5000);
